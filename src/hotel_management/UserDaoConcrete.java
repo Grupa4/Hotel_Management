4d61e5fa5f9a4dc23041c8aa2de0b639e5ac114b
Amel Alispahic <alispahicamel@hotmail.com>
2016-03-10 16:34:35 +0100

    Nove dvije klase

---
+++ src/hotel_management/UserDaoConcrete.java
@@ -0,0 +1,36 @@
+package hotel_management;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+
+
+public class UserDaoConcrete implements UserDao{
+
+	@Override
+	public void updateUser() throws SQLException {
+		
+	}
+
+	@Override
+	public void dodajUser() throws SQLException {
+		
+	}
+
+	@Override
+	public void odjaviUser() throws SQLException {
+		
+	}
+
+	@Override
+	public void pretraziUsers() throws SQLException {
+		
+	}
+
+	@Override
+	public ArrayList<User> getUsers() throws SQLException {
+		return null;
+	}
+
+	
+}
