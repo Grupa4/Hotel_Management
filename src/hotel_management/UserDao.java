228ca338fee5cb89d7f4fdf0a5c5f79841b84fa3
cecilija <cecilijapetrovic@yahoo.com>
2016-03-11 17:27:37 +0100

    samo sam dodala u zagradi type i name

--- src/hotel_management/UserDao.java
+++ src/hotel_management/UserDao.java
@@ -4,11 +4,18 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public interface UserDao {
-	
-	public void updateUser() throws SQLException;
-	public void dodajUser() throws SQLException;
-	public void odjaviUser() throws SQLException;
-	public void pretraziUsers() throws SQLException;
-	public ArrayList<User> getUsers() throws SQLException;	
-	
+
+	// ispravila sam ovdje u zagradama samo dodala podatak po kojem ce metoda da
+	// radi. znaci update po objektu a odjavit mozemo i na osnovu imena kao i
+	// pretrazit
+	public void updateUser(User gost) throws SQLException;
+
+	public void dodajUser(User gost) throws SQLException;
+
+	public void odjaviUser(String name) throws SQLException;
+
+	public void pretraziUsers(String name) throws SQLException;
+
+	public ArrayList<User> getUsers() throws SQLException;
+
 }
